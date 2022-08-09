c51510434bc8b6e2aa1e81418dcf146cc0ac3c60
Vuillaume Nicolas FRSS <nicolas.vuillaume@syngenta.com>
2022-08-09 17:49:00 +0000

    modified:   ML/_nv/yolov1/loss_to_recode.py

--- ML/_nv/yolov1/loss_to_recode.py
+++ ML/_nv/yolov1/loss_to_recode.py
@@ -71,6 +71,7 @@ class YoloLossRC(nn.Module):
             torch.flatten(box_targets, end_dim=-2),
         )
         #print(f"box_loss{box_loss}")
+
         # ==================== #
         #   FOR OBJECT LOSS    #
         # ==================== #
